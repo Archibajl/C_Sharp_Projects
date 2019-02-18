1d2907d74522d5f4736bf5635c22785a9661429e
j Archi <Naruto7721@msn.com>
2019-02-17 17:57:56 -0700

    UWOT

---
+++ HW3_Archibald/HW3_Archibald/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Console;
+
+
+namespace HW3_Archibald
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = @"C://";
+            WriteLine("To search and copy enter a directory to search.");
+            directory += ReadLine();
+        }
+
+        string ReturnFilePath(string start, string fileType)
+        {
+            string output = null;
+
+            foreach (string directory in Directory.GetDirectories(start))
+            {
+                foreach( string files in Directory.GetFiles(directory, fileType))
+                {
+                    output = files;
+                }
+            }
+
+            return output;
+        }
+    }
+}
