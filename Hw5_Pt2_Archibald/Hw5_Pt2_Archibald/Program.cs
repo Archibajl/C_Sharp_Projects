ca1757a4264c06bf3619da3a6ef2115ea8f440c1
j Archi <Naruto7721@msn.com>
2019-03-05 02:25:53 -0700

    j

--- Hw5_Pt2_Archibald/Hw5_Pt2_Archibald/Program.cs
+++ Hw5_Pt2_Archibald/Hw5_Pt2_Archibald/Program.cs
@@ -10,10 +10,91 @@ namespace Hw5_Pt2_Archibald
     {        
         static void Main(string[] args)
         {
-            SolutionsLinq sl = new SolutionsLinq();
+            Program p = new Program();
+            SolutionsLinq Sql = new SolutionsLinq();
+            //SolutionsLinq GetA = new void SolutionsLinq.GetStraightAStudents();
+            List<Student> stt = new List<Student>();
 
-            sl.GetStraightAStudents();
+            Console.WriteLine($" Get all straight a students in a list.");
+            Sql.GetStraightAStudents();
+            Console.WriteLine($" The item which is most profitable: {DetermineWhichItemWasMostProfitable().ItemName}");
+            Console.WriteLine($"The sum of all prime numbers between 2 and 1000 is {p.SumPrimes()}");
 
+
+            Console.ReadKey();
+        }
+
+        
+
+        //Returns which item is most profitable.
+        public static Item DetermineWhichItemWasMostProfitable()
+        {
+            //list to store initial values.
+            List<Item> items = new List<Item>()
+            {
+                new Item("TV", 5, 515.15f),
+                new Item("DVD Player", 10, 100.05f),
+                new Item("Toy Horse", 2, 25.25f),
+                new Item("Shovel", 1, 10.99f),
+                new Item("Kite", 4, 5.77f),
+                new Item("Stapler", 15, 5.98f),
+                new Item("Pen", 25, 2.25f),
+                new Item("Candy Bar", 102, 1.5f),
+                new Item("DVD", 45, 20),
+                new Item("Soda", 66, 1.5f),
+            };
+
+            List<Item> highestEarner = new List<Item>();
+            
+            //uses a lambda expression to check values against all other values in the list.
+            var ItemMostProf =                                        
+                from item in items
+                where true == items.All(x =>  ( x.ItemPrice * x.NumberOfSales ) <= ( item.ItemPrice * item.NumberOfSales ) )
+                select item;
+            //uses a list to output values.
+            highestEarner = ItemMostProf.ToList<Item>();
+            Item HighestEarner = new Item("Temp", 0, 0);
+
+            HighestEarner = highestEarner[0];
+            
+                return HighestEarner;
+        }
+
+        // Finds and sums the values of all primes between 2 and 1,000.
+        public int SumPrimes()
+        {
+            IEnumerable<int> Numbers = Enumerable.Range(2, 1000);
+
+            //Checks if each number is a prime number.
+            //Tried nesting a linq and setting a variable to number % of all numbers up to number != 0 and a lambda that should do the same thing.
+            //Neither returned any results, so i went back to using an outside function with a linq.
+            var PrimeNumbers =
+            from number in Numbers
+            where Prime(number) == true
+            select number;           
+
+            //Returns the sums of the output.
+            int primeSums = PrimeNumbers.Sum();
+            return primeSums;
+        }
+        bool Prime(int Num)
+        {
+            //resizes the range of numbers up to the number being checked
+            IEnumerable<int> Numbers2 = Enumerable.Range(2, Num);
+            bool selector = true;
+
+
+            //returns the number of numbers that each itteration is true for.
+            var truth =
+                 from prime in Numbers2
+                 where Num % prime != 0
+                 select 1;
+
+            //checks if the same number are not evenly divisible by any other number.
+            selector = (truth.Sum() == Num - 1);
+
+            return selector;
         }
     }
 }
+
--- Hw5_Pt2_Archibald/Hw5_Pt2_Archibald/SolutionsLinq.cs
+++ Hw5_Pt2_Archibald/Hw5_Pt2_Archibald/SolutionsLinq.cs
@@ -9,31 +9,8 @@ namespace Hw5_Pt2_Archibald
 {
     class SolutionsLinq
     {
-        
-        //Returns any string in a that isn't also in b.
-        public static List<string> FindStringsInAThatArentInB()
-        {
-            List<string> a = new List<string>() { "r", "y", "a", "n" };
-            List<string> b = new List<string>() { "d", "a", "r", "r", "a", "s" };
-            List<string> result = new List<string>();
-
-            result =
-            from wordA in a
-            let TruthVal = new
-            {
-                retval =
-                from wordB in b
-                where wordA != wordB
-                select 1
-            }
-            where TruthVal.All(g => g >= 1)
-            select wordA;
-
-            return result;
-        }
-
         //Returns students that have straight A's.
-        public static List<Student> GetStraightAStudents()
+        public void GetStraightAStudents()
         {
             List<Student> students = new List<Student>()
             {
@@ -49,6 +26,8 @@ namespace Hw5_Pt2_Archibald
                 new Student(10, 85, 92, 93, 95),
             };
 
+            List<Student> a_students = new List<Student>();
+
             {
                 var AStudents =
                 from student in students
@@ -57,67 +36,98 @@ namespace Hw5_Pt2_Archibald
                 where student.ScienceGrade >= 90
                 select student;
 
-                return AStudents.ToList<Student>();
-            }           
+                a_students.AddRange(AStudents);
+
+                foreach (var astudent in a_students)
+                {
+                    Console.WriteLine("Student ID" + astudent.StudentID + "Is an A student.");
+                }
+
+            }
         }
 
-        //Returns which item is most profitable.
-        public static Item DetermineWhichItemWasMostProfitable()
+
+        //Returns any string in a that isn't also in b.
+        public static List<string> FindStringsInAThatArentInB()
         {
-            List<Item> items = new List<Item>()
+            List<string> a = new List<string>() { "r", "y", "a", "n" };
+            List<string> b = new List<string>() { "d", "a", "r", "r", "a", "s" };
+            List<string> result = new List<string>();
+
+            result =
+            from wordA in a
+            let TruthVal = new
             {
-                new Item("TV", 5, 515.15f),
-                new Item("DVD Player", 10, 100.05f),
-                new Item("Toy Horse", 2, 25.25f),
-                new Item("Shovel", 1, 10.99f),
-                new Item("Kite", 4, 5.77f),
-                new Item("Stapler", 15, 5.98f),
-                new Item("Pen", 25, 2.25f),
-                new Item("Candy Bar", 102, 1.5f),
-                new Item("DVD", 45, 20),
-                new Item("Soda", 66, 1.5f),
+                retval =
+                from wordB in b
+                where wordA != wordB
+                select true
+            }
+            where TruthVal(g => g == true)
+            select wordA;
+
+            return result;
+        }
+
+        /// <summary>
+        /// HW 3. Finds all jpgs in the given path
+        /// 
+        /// You will need to change your path in multiple locations to test this.
+        /// 1. In Main of this file.
+        /// 2. In GetAllPNG_Test in LINQProblems_Test_Cases.cs
+        /// 
+        /// PLEASE CHANGE BOTH BACK TO @"C:\Users\Ryan\Pictures\" WHEN YOU FINALIZE AND SUMBIT!!!!
+        /// </summary>
+        public static List<FileInfo> GetAllPNG(string path)
+        {
+            /* Hints for this problem:
+             * new DirectoryInfo(path).EnumerateDirectories("*", SearchOption.AllDirectories);
+             * directory.EnumerateFiles("*.png")
+             * 
+             * However, the enumerated method above might throw exceptions on files that cannot be accessed.
+             * To solve this, just test on a path that you know is safe.
+             */
+
+            List<FileInfo> files = new List<FileInfo>();
+
+            // from DirectoryInfo directory in new DirectoryInfo(path).GetDirectories()
+            //    select new
+            //    {
+            //        files.AddRange(GetAllPNG(directory.FullName));
+
+            //}
+            //foreach (DirectoryInfo directory in new DirectoryInfo(path).GetDirectories())
+            //{
+            //    files.AddRange(GetAllPNG(directory.FullName));
+            //}
+
+            var NewFiles =
+            from DirectoryInfo file in new DirectoryInfo(path).GetDirectories()
+            where file.Extension == ".png"
+            select new {
+               select  files.Add(file.ToList)
             };
 
-            var ItemMostProf = 
-            from item in items
-            where items.All(x => x.NumberOfSales * x.ItemPrice > item.NumberOfSales * item.ItemPrice )
-            select item; 
+            //files.AddRange(NewFiles.ToList<FileInfo>());
+            return files;
 
-            return ItemMostProf.ToArray<Item>();
         }
-
         //Gets the fibonacci numbers for a list of integers.
         public static List<int> GetFibonacciNumbers()
         {
-            List<int> input = new List<int>();
+            //Fills the list.
+            List<int> Input = new List<int>();
+            for (int i = 1; i < 40; i += 5)
+                input.Add(i);
+            //outputvalue
+            List<int> results = new List<int>();
 
-        }
-
-            // Finds and sums the values of all primes between 2 and 1,000.
-            public static int SumPrimes()
-            {
-                IEnumerable<int> Numbers = Enumerable.Range(2, 1000);
-                IEnumerable<int> Numbers2 = Enumerable.Range(2, 1000);
-
-
-            var PrimeNumbers =
-            from number in Numbers
-            let Output = new
-             {
-                 Out =
-                from num in Numbers2 where num <= number
-                where number % num != 0
-                select 1
-            }
-            where Convert.ToInt32(Output) == number
-            select number;
-
-            return PrimeNumbers.Sum();
-            }
+            from number in Input
+            where (number == 1) || (number == 2)
+            select 
 
         }
 
 
-
-       
+    }
 }
