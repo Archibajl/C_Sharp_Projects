1397a1e6f155a549766c0b013f42d919db5faefe
Archibajl <Naruto7721@msn.com>
2019-03-11 23:05:51 +0000

    Add files via upload

---
+++ Midterm_Students/Midterm_Students/Midterm/VendingMachineOption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    abstract class VendingMachineOption : VendingMachine
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }      
+
+        public VendingMachineOption(string name, float price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
