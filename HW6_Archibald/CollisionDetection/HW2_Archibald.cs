eff925384852470b0b6c968a55b7f9dd1145384c
j Archi <Naruto7721@msn.com>
2019-03-29 00:54:02 -0600

    Pi Updated Idea on how to paralellize hw6

--- HW6_Archibald/CollisionDetection/HW2_Archibald.cs
+++ HW6_Archibald/CollisionDetection/HW2_Archibald.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace CollisionDetection
 {
-    static class Program
+    static class HW2_Archibald
     {
         /// <summary>
         /// The main entry point for the application.
--- MultiThreading_PI Lab/MultiThreading_PI Lab/Program.cs
+++ MultiThreading_PI Lab/MultiThreading_PI Lab/Program.cs
@@ -18,34 +18,40 @@ namespace MultiThreading_PI_Lab
             int oddFraction4 = 400001;
 
             Thread Calculator1 = new Thread(() => {
-                for(int i =0; i<= 100000; i+= 2)
+                for(int i =3; i<= 100000; i+= 2)
                 {
                     oddFraction1 += oddFraction1 + 2;
-                }
-                Pi += oddFraction1/10000;
+                    Pi += (1 / Convert.ToDouble(oddFraction1));
+                }                
             });
             Thread Calculator2 = new Thread(() => {
-                for (int i = 0; i <= 100000; i += 2)
+                for (int i =100001; i <= 200000; i += 2)
                 {
                     oddFraction2 += oddFraction2 + 2;
-                }
-                Pi += oddFraction2 / 200000;
+                    Pi += ( 1/ Convert.ToDouble(oddFraction2));
+                }                
             });
             Thread Calculator3 = new Thread(() => {
-                for (int i = 0; i <= 100000; i += 2)
+                for (int i = 200001; i <= 300000; i += 2)
                 {
                     oddFraction3 += oddFraction3 + 2;
-                }
-                Pi += oddFraction2 / 300000;
+                    Pi += (1 / Convert.ToDouble(oddFraction3));
+                }                
             });
             Thread Calculator4 = new Thread(() => {
-                for (int i = 0; i <= 100000; i += 2)
+                for (int i = 300001; i <= 400000; i += 2)
                 {
                     oddFraction4 += oddFraction4 + 2;
-                }
-                Pi += oddFraction2 / 400000;
+                    Pi += (1 / Convert.ToDouble(oddFraction4));
+                }                
             });
 
+            Calculator1.Start();
+            Calculator2.Start();
+            Calculator3.Start();
+            Calculator4.Start();
+            Console.WriteLine($"{Pi}");
+            Console.ReadKey();
         }
     }
 }
