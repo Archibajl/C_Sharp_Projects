f42ad5cf6acddb88e4f256aeebd4011e211edc1d
j Archi <Naruto7721@msn.com>
2019-05-05 23:40:58 -0600

    j

--- HW8_MultiplayerTicTacToe/HW8_MultiplayerTicTacToe/Form1.Designer.cs
+++ HW8_MultiplayerTicTacToe/HW8_MultiplayerTicTacToe/Form1.Designer.cs
@@ -28,20 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.txt_IpConnection = new System.Windows.Forms.TextBox();
+            this.btn_ResetGame = new System.Windows.Forms.Button();
+            this.btn_TestConnection = new System.Windows.Forms.Button();
+            this.lbl_Connection = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tic Tac Toe";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(46, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(68, 62);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(120, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 62);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(194, 107);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(68, 62);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(46, 175);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(68, 62);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(120, 175);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(68, 62);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(194, 175);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(68, 62);
+            this.textBox6.TabIndex = 6;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(46, 243);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(68, 62);
+            this.textBox7.TabIndex = 7;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(120, 243);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(68, 62);
+            this.textBox8.TabIndex = 8;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.Location = new System.Drawing.Point(194, 243);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(68, 62);
+            this.textBox9.TabIndex = 9;
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox9.TextChanged += new System.EventHandler(this.textBox9_TextChanged);
+            // 
+            // txt_IpConnection
+            // 
+            this.txt_IpConnection.Location = new System.Drawing.Point(33, 349);
+            this.txt_IpConnection.Name = "txt_IpConnection";
+            this.txt_IpConnection.Size = new System.Drawing.Size(100, 20);
+            this.txt_IpConnection.TabIndex = 10;
+            // 
+            // btn_ResetGame
+            // 
+            this.btn_ResetGame.Location = new System.Drawing.Point(206, 390);
+            this.btn_ResetGame.Name = "btn_ResetGame";
+            this.btn_ResetGame.Size = new System.Drawing.Size(75, 23);
+            this.btn_ResetGame.TabIndex = 11;
+            this.btn_ResetGame.Text = "Reset Game";
+            this.btn_ResetGame.UseVisualStyleBackColor = true;
+            this.btn_ResetGame.Click += new System.EventHandler(this.btn_ResetGame_Click);
+            // 
+            // btn_TestConnection
+            // 
+            this.btn_TestConnection.Location = new System.Drawing.Point(33, 431);
+            this.btn_TestConnection.Name = "btn_TestConnection";
+            this.btn_TestConnection.Size = new System.Drawing.Size(100, 23);
+            this.btn_TestConnection.TabIndex = 12;
+            this.btn_TestConnection.Text = "Test Connection";
+            this.btn_TestConnection.UseVisualStyleBackColor = true;
+            this.btn_TestConnection.Click += new System.EventHandler(this.btn_TestConnection_Click);
+            // 
+            // lbl_Connection
+            // 
+            this.lbl_Connection.AutoSize = true;
+            this.lbl_Connection.Location = new System.Drawing.Point(30, 387);
+            this.lbl_Connection.Name = "lbl_Connection";
+            this.lbl_Connection.Size = new System.Drawing.Size(121, 26);
+            this.lbl_Connection.TabIndex = 13;
+            this.lbl_Connection.Text = "Ip Adress to Connect to.\r\nDefaults to current IP";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 493);
+            this.ClientSize = new System.Drawing.Size(330, 482);
+            this.Controls.Add(this.lbl_Connection);
+            this.Controls.Add(this.btn_TestConnection);
+            this.Controls.Add(this.btn_ResetGame);
+            this.Controls.Add(this.txt_IpConnection);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Multiplayer Tic Tac Toe";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox txt_IpConnection;
+        private System.Windows.Forms.Button btn_ResetGame;
+        private System.Windows.Forms.Button btn_TestConnection;
+        private System.Windows.Forms.Label lbl_Connection;
     }
 }
 
--- HW8_MultiplayerTicTacToe/HW8_MultiplayerTicTacToe/Form1.cs
+++ HW8_MultiplayerTicTacToe/HW8_MultiplayerTicTacToe/Form1.cs
@@ -7,14 +7,290 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
 
 namespace HW8_MultiplayerTicTacToe
 {
     public partial class Form1 : Form
     {
+        CheckGame cg = new CheckGame();
+        char[,] Board = new char[3,3];
+        char FinalVal;
+        //List<TcpClient> connections = new List<TcpClient>();
+        TcpClient Player;
+
         public Form1()
         {
             InitializeComponent();
+            txt_IpConnection.Text = Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString();
+        }    
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (Return(textBox1, 0, 0))
+            {
+                if (IsFin() == false)
+                {
+                    string val = Board[0,0].ToString();
+                    SendMessage( Player ,"0", "0", val);
+                    Listen();
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {            
+            if (Return(textBox2, 0, 1))
+            {
+                if (IsFin() == false)
+                {
+                    string val = Board[0,1].ToString();
+                    SendMessage(Player, "0", "1", val);
+                    Listen();
+                }
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {           
+            if (Return(textBox3, 0, 2))
+            {
+                if (IsFin() == false)
+                {
+                    string val = Board[0,2].ToString();
+                    SendMessage(Player, "0", "2", val);
+                    Listen();
+                }
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (Return(textBox4, 1, 0))
+            {
+                if (IsFin() == false)
+                {
+                    string val = Board[1,0].ToString();
+                    SendMessage(Player, "1", "0", val);
+                    Listen();
+                }
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            bool goodVal = Return(textBox5, 1, 1);
+            if (goodVal == true)
+            {
+                if (IsFin() == false)
+                {
+                    string val = Board[1,1].ToString();
+                    SendMessage(Player, "1", "1", val);
+                    Listen();
+                }
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            bool goodVal = Return(textBox6, 1, 2);
+            if (goodVal == true)
+            {
+                if (IsFin() == false)
+                {
+                    string val = Board[1,2].ToString();
+                    SendMessage(Player, "1", "2", val);
+                    Listen();
+                }
+            }
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            bool goodVal = Return(textBox7, 2, 0);
+            if (goodVal == true)
+            {
+                if (IsFin() == false)
+                {
+                    string val = Board[2, 0].ToString();
+                    SendMessage(Player, "2", "0", val);
+                    Listen();
+                }
+
+            }
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            bool goodVal = Return(textBox8, 2, 1);
+            if (goodVal == true)
+            {
+                if (IsFin() == false)
+                {
+                    string val = Board[2,1].ToString();
+                    SendMessage(Player, "2", "1", val);
+                    Listen();
+                }
+            }
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            bool goodVal = Return(textBox9, 2, 2);
+            if (goodVal == true)
+            {
+                if (IsFin() == false)
+                {
+                    string val = Board[2,2].ToString();
+                    SendMessage(Player, "2", "2", val);
+                    Listen();
+                }
+            }            
+        }
+
+        private bool Return(TextBox Box, int loc1, int loc2)
+        {
+            string input = Box.Text;
+            input = input.ToUpper();
+
+            if ((input == "O") || (input == "X"))
+            {
+                Box.Enabled = false;
+                Box.Text = input;
+                Board[loc1, loc2] = char.Parse(input);
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Enter either an X or an O");
+                Box.Text = "";
+                return false;
+            }
+            
+        }
+
+        bool IsFull()
+        {
+            List<TextBox> Boxes = TextBoxes();
+            for(int i = 0; i < Boxes.Count; i++)
+            {
+                if (Boxes[i].Text == null)
+                    return false;
+            }
+
+            return true;
+        }
+
+        bool IsFin()
+        {
+            List<TextBox> Box = TextBoxes();
+            if (IsFull())
+            {
+                if (cg.CheckBoard(Board, ref FinalVal))
+                {
+                    label1.Text = "Game Over";
+                    GrayBoxes(Box, false);
+                    MessageBox.Show($"{FinalVal}'s Win");
+                    return true;
+                }
+                else
+                {
+                    label1.Text = "Game Over";
+                    return true;
+                }
+            }
+            else
+            {
+                if (cg.CheckBoard(Board, ref FinalVal))
+                {
+                    label1.Text = "Game Over";
+                    GrayBoxes(Box, false);
+                    MessageBox.Show($"{FinalVal}'s Win");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        void GrayBoxes(List<TextBox> Boxes, bool enable)
+        {
+            for (int i = 0; i< Boxes.Count; i++)
+            {
+                Boxes[i].Enabled = enable;
+                if(enable == true)
+                {
+                    Boxes[i].Text = "";
+                }
+            }
+        }
+
+        List<TextBox> TextBoxes()
+        {
+            List<TextBox> retBoxes = new List<TextBox>
+            {
+                textBox1, textBox2, textBox3, textBox4, textBox5, textBox6, textBox7, textBox8, textBox9 
+            };
+
+            return retBoxes;
+        }
+
+        async void Listen()
+        {
+            try
+            {
+                Player = new TcpClient(Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToString(), 5555);
+            }
+            catch
+            {                
+                TcpListener listener = new TcpListener(Dns.GetHostEntry(Dns.GetHostName()).AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork), 5555);
+                listener.Start();
+                Player = await listener.AcceptTcpClientAsync();
+                await Task.Factory.StartNew(() => ListenForPacket(Player));
+                listener.Stop();
+                return;
+            }            
+            await Task.Factory.StartNew(() => ListenForPacket(Player));
+        }
+
+        private void ListenForPacket(TcpClient connection)
+        {
+            NetworkStream stream = connection.GetStream();
+            while (true)
+            {
+                byte[] bytesToRead = new byte[connection.ReceiveBufferSize];
+                int bytesRead = stream.Read(bytesToRead, 0, connection.ReceiveBufferSize);
+                string result = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+                if (result != "")
+                {
+                    char[] res = result.ToCharArray();
+                    int position1 = res[0];
+                    int position2 = res[1];
+                    Board[position1, position2] = res[2];
+                }
+            }
+        }
+
+        private void SendMessage(TcpClient Connection, string Loc1, string Loc2, string Val)
+        {
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(Loc1 + Loc2 + Val);
+            Connection.GetStream().Write(bytesToSend, 0, bytesToSend.Length);
+        }
+
+        private void btn_ResetGame_Click(object sender, EventArgs e)
+        {
+            List<TextBox> Box = TextBoxes();
+            label1.Text = "Tic Tac Toe";
+            GrayBoxes( Box, true);
+        }
+
+        private void btn_TestConnection_Click(object sender, EventArgs e)
+        {            
+            Listen();
+            //SendMessage(Player, "0", "0", "0");
         }
     }
 }
